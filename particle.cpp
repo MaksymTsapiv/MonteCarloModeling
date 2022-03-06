d3982ee9da9679a2c64484e9579f60f1bae88c0f
Maksym Tsapiv <maksym.tsapiv@ucu.edu.ua>
2022-03-06 18:04:39 +0200

    Add particle

---
+++ particle.cpp
@@ -0,0 +1,36 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include "particle.h"
+
+double Particle::get_x() {
+    return x_cor;
+}
+
+double Particle::get_y() {
+    return y_cor;
+}
+
+double Particle::get_z() {
+    return z_cor;
+}
+
+double Particle::get_sigma() {
+    return sigma;
+}
+
+void Particle::set_x(double x) {
+    x_cor = x;
+}
+
+void Particle::set_y(double y) {
+    y_cor = y;
+}
+
+void Particle::set_z(double z) {
+    z_cor = z;
+}
+
+void Particle::set_sigma(double new_sigma) {
+    sigma = new_sigma;
+}
---
+++ particle.h
@@ -0,0 +1,24 @@
+#ifndef PARTICLE_FILE_H
+#define PARTICLE_FILE_H
+
+class Particle {
+private:
+    double x_cor, y_cor, z_cor, sigma;
+
+public:
+    Particle() : x_cor(0), y_cor(0), z_cor(0), sigma(0) {};
+    Particle(double x, double y, double z, double sigma) : x_cor(x), y_cor(y), z_cor(z), sigma(sigma) {};
+
+    double get_x();
+    double get_y();
+    double get_z();
+    double get_sigma();
+
+    void set_x(double x);
+    void set_y(double y);
+    void set_z(double z);
+    void set_sigma(double new_sigma);
+};
+
+
+#endif //PARTICLE_FILE_H
