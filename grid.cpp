26c88077fffeabd2a4ef9bd129ef0147b9b976fe
Archy <archy.revera@protonmail.com>
2022-03-07 14:30:10 +0200

    Add grid getters and setters

--- grid.cpp
+++ grid.cpp
@@ -1,5 +1,24 @@
 #include "grid.h"
 
+double Grid::get_Lx() {
+    return Lx;
+}
+double Grid::get_Ly() {
+    return Ly;
+}
+double Grid::get_Lz() {
+    return Lz;
+}
+
+void Grid::set_Lx(double x) {
+    Lx = x;
+}
+void Grid::set_Ly(double y) {
+    Ly = y;
+}
+void Grid::set_Lz(double z) {
+    Lz = z;
+}
 
 void Grid::fill(size_t n) {
 
--- grid.h
+++ grid.h
@@ -11,8 +11,18 @@ private:
     double Lx, Ly, Lz;
     std::vector<Particle> particles{};
 
-
 public:
+    Grid() : Lx(0), Ly(0), Lz(0) {};
+    Grid(double x, double y, double z) : Lx(x), Ly(y), Lz(z) {};
+
+    double get_Lx();
+    double get_Ly();
+    double get_Lz();
+
+    void set_Lx(double x);
+    void set_Ly(double y);
+    void set_Lz(double z);
+
     void fill(size_t n);
     void move();
 };
