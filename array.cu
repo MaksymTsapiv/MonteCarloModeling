e3660178bd41b77ea7a9158dec7c5fa73d0797e3
Maksym Tsapiv <maksym.tsapiv@ucu.edu.ua>
2022-04-16 12:33:53 +0300

    fix

--- array.cu
+++ array.cu
@@ -20,8 +20,8 @@ void OrderedArray::insert(Particle value, size_t index) {
     if (index > size) {
         throw std::out_of_range("index out of range");
     }
-    for (size_t i = size; i > index; --i) {
-        data[i] = data[i - 1];
+    for (size_t i = index; i < size; ++i) {
+        data[i] = data[i + 1];
     }
     data[index] = value;
     ++size;
